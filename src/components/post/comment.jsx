5e4b01ae86875fb7b413228d9f1a899f618a4214
Hristo Koev <iamhristokoev@gmail.com>
2023-11-09 00:36:08 +0100

    bring back body content

--- src/components/post/comment.jsx
+++ src/components/post/comment.jsx
@@ -3,7 +3,7 @@ export default function Comment({ comment }) {
 		<li className="relative space-y-5">
 			<div className="flex items-start">
 				<div className="p-4 bg-slate-100 rounded-md">
-					{/* <div className="grow text-sm text-slate-800 space-y-2 mb-2" dangerouslySetInnerHTML={{ __html: comment.body }} /> */}
+					<div className="grow text-sm text-slate-800 space-y-2 mb-2" dangerouslySetInnerHTML={{ __html: comment.body }} />
 					<div className="flex flex-wrap text-xs">
 						<div className="flex items-center after:block after:content-['·'] last:after:content-[''] after:text-sm after:text-slate-400 after:px-2">
 							<span className="font-medium text-slate-500">
