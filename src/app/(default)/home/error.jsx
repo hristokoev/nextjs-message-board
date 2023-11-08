6348424eb49367a533693f854189088f76e43564
Hristo Koev <iamhristokoev@gmail.com>
2023-11-08 22:24:46 +0100

    remove error pages

--- src/app/(default)/home/error.jsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export default async function ErrorHome() {
-	return (
-		<div>
-			<h1>Error!</h1>
-		</div>
-	)
-}
--- src/app/(default)/post/[id]/error.jsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export default async function ErrorPost() {
-	return (
-		<div>
-			<h1>Error!</h1>
-		</div>
-	)
-}
