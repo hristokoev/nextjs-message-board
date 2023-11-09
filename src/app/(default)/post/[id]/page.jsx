3b7273797a19a2bf76701fb52eb03fc1bd67e883
Hristo Koev <iamhristokoev@gmail.com>
2023-11-09 08:44:59 +0100

    loading state not showing with async generateMetadata in page.jsx, see issue #55524 on @vercel/next.js; reverting

--- src/app/(default)/post/[id]/page.jsx
+++ src/app/(default)/post/[id]/page.jsx
@@ -1,15 +1,6 @@
 import { fetchSinglePost } from '@/lib/fetchData'
 import SinglePost from '@/components/post/single-post'
 
-export async function generateMetadata( { params: { id } } ) {
-	const post = await fetchSinglePost(id)
-
-	return {
-		title: `${post.title} - Board`,
-		description: `${post.body.slice(0, 100)}... Read the full text on the post.`,
-	}
-}
-
 export default async function Post({ params: { id } }) {
 
 	const post = await fetchSinglePost(id)
