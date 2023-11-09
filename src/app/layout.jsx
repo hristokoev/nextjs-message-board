51bdff5aa99cb21888a1e145bff7b5ae9e531fde
Hristo Koev <iamhristokoev@gmail.com>
2023-11-09 01:12:39 +0100

    add vercel analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 import './css/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
 		<html lang="en">
 			<body className={inter.className}>
 				{children}
+				<Analytics />
 			</body>
 		</html>
 	)
